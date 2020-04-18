f0bb2cfc9536ff9f0d2a59dedc43afbf737060c4
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-04-18 17:08:00 +0100

    Added tests for Rounding types: CEIL and FLOOR

--- tests/grim_tester_test.py
+++ tests/grim_tester_test.py
@@ -64,3 +64,45 @@ def test_simple_rounding_half_down(mean, n, expected_consistency):
     assert calculated_consistency == expected_consistency, \
         f"The calculated score was: {calculated_consistency}, the expected score: {expected_consistency}. " + \
         f"The mean was: {mean} and the population size (n) was: {n}"
+
+
+@pytest.mark.parametrize("mean, n, expected_consistency", [
+    ('11.67', '3', True),
+    ('11.09', '21', False),
+    ('11.09', '22', False),
+    ('133.98', '28', False),
+    ('-11.67', '3', False),
+    ('-11.66', '3', True),
+    ('-11.6667', '3', False),
+    ('-11.6666', '3', True),
+    ('-11.09', '21', True),
+    ('-11.09', '22', True),
+    ('-133.98', '28', False)
+])
+def test_simple_rounding_ceiling(mean, n, expected_consistency):
+
+    calculated_consistency = grim_tester.consistency_check(mean, n, decimal.ROUND_CEILING)
+    assert calculated_consistency == expected_consistency, \
+        f"The calculated score was: {calculated_consistency}, the expected score: {expected_consistency}. " + \
+        f"The mean was: {mean} and the population size (n) was: {n}"
+
+
+@pytest.mark.parametrize("mean, n, expected_consistency", [
+   ('11.67', '3', False),
+   ('11.09', '21', True),
+   ('11.09', '22', True),
+    ('133.98', '28', False),
+    ('-11.67', '3', True),
+    ('-11.66', '3', False),
+    ('-11.6667', '3', True),
+    ('-11.6666', '3', False),
+    ('-11.09', '21', False),
+    ('-11.09', '22', False),
+    ('-133.98', '28', False)
+])
+def test_simple_rounding_floor(mean, n, expected_consistency):
+
+    calculated_consistency = grim_tester.consistency_check(mean, n, decimal.ROUND_FLOOR)
+    assert calculated_consistency == expected_consistency, \
+        f"The calculated score was: {calculated_consistency}, the expected score: {expected_consistency}. " + \
+        f"The mean was: {mean} and the population size (n) was: {n}"
