6b30ce11f336d4d5cf2ea2ffa4a1a25306ce8bb3
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-04-26 14:48:50 +0100

    Folder & test file reorg

---
+++ tests/grim_tester_exceptions.py
@@ -0,0 +1,27 @@
+import pytest
+from grim import grim_tester
+import decimal
+
+
+@pytest.mark.parametrize("mean, n", [
+        ('10.00', '-1'),
+        ('11.10', '0'),
+        ('-4511.67', '-982743875438547')
+])
+def test_n_value_exceptions_for_negatives(mean, n):
+
+    with pytest.raises(ValueError, match=r".*greater than zero.*"):
+        grim_tester.consistency_check(mean, n)
+
+
+@pytest.mark.parametrize("mean, n", [
+        ('10.00', '1.01'),
+        ('11.10', '0.5'),
+        ('11.10', '0.000000003345'),
+        ('-4511.67', '98274387.5438547')
+])
+def test_n_value_exceptions_for_integer(mean, n):
+
+    with pytest.raises(ValueError, match=r".*whole number.*"):
+        grim_tester.consistency_check(mean, n)
+
