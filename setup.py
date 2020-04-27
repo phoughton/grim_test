c9ba4e1f472c57e22f457abf1a687906e23d9404
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-04-27 15:07:45 +0100

    Case change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Grim",
+    name="grim",
     version="0.0.1",
     author="Peter Houghton",
     author_email="pete@investigatingsoftware.co.uk",
