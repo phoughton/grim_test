f6cbf77fbc6e483c4590481cee757ba4da935391
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-05-01 16:28:46 +0100

    Switch to use CSV and pandas to store test data

--- tests/grim_tester_test.py
+++ tests/grim_tester_test.py
@@ -1,203 +1,62 @@
 import pytest
 from grim import mean_tester
 import decimal
+import pandas as pd
+import os
+
+
+rounding_types = ["ROUND_CEILING",
+                  "ROUND_DOWN",
+                  "ROUND_FLOOR",
+                  "ROUND_HALF_DOWN",
+                  "ROUND_HALF_EVEN",
+                  "ROUND_HALF_UP",
+                  "ROUND_UP",
+                  "ROUND_05UP"]
+
+TEST_DATA_FILE = "grim_test_data.csv"
 
 
 def check_consistency(mean, n, expected_consistency, rounding_type):
     calculated_consistency = mean_tester.consistency_check(mean, n, rounding_type)
     assert calculated_consistency == expected_consistency, \
-        f"The calculated score was: {calculated_consistency}, the expected score: {expected_consistency}. " + \
+        f"The calc consistency was: {calculated_consistency}, the expected consistency: {expected_consistency}. " + \
         f"The mean was: {mean} and the population size (n) was: {n}. (Passed as Decimals). " + \
-        f"The rounding type was ${rounding_type}.  "
+        f"The rounding type was {rounding_type}.  "
 
-    calculated_consistency = mean_tester.consistency_check(decimal.Decimal(mean), decimal.Decimal(n), rounding_type)
+    expected_consistency = mean_tester.consistency_check(decimal.Decimal(mean), decimal.Decimal(n), rounding_type)
     assert calculated_consistency == expected_consistency, \
-        f"The calculated score was: {calculated_consistency}, the expected score: {expected_consistency}. " + \
+        f"The calc consistency: {calculated_consistency}, the expected consistency: {expected_consistency}. " + \
         f"The mean was: {mean} and the population size (n) was: {n}.  (Passed as strings). " + \
-        f"The rounding type was ${rounding_type}.  "
+        f"The rounding type was {rounding_type}.  "
 
 
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', True),
-    ('11.09', '21', False),
-    ('11.09', '22', True),
-    ('11.09', '23', True),
-    ('0.3333333333333', '9', True),
-    ('133.98', '28', False),
-    ('-11.67', '3', True),
-    ('-11.66', '3', False),
-    ('-11.09', '21', False),
-    ('-11.09', '22', True),
-    ('-11.09', '23', True),
-    ('-133.98', '28', False)
-])
-def test_simple_no_rounding_defined(mean, n, expected_consistency):
+@pytest.fixture()
+def data_loader():
+    test_data = pd.read_csv(os.path.join(os.path.dirname(__file__), TEST_DATA_FILE), dtype={'mean': str, 'n': str})
 
-    calculated_consistency = mean_tester.consistency_check(mean, n)
-    assert calculated_consistency == expected_consistency, \
-        f"The calculated score was: {calculated_consistency}, the expected score: {expected_consistency}. " + \
-        f"The mean was: {mean} and the population size (n) was: {n}"
+    test_sets = []
 
+    for rounding_type in rounding_types:
+        for row_index in range(0, len(test_data)):
 
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', True),
-    ('11.09', '21', False),
-    ('11.09', '22', True),
-    ('11.09', '23', True),
-    ('0.3333333333333', '9', True),
-    ('133.98', '28', False),
-    ('-11.67', '3', True),
-    ('-11.66', '3', False),
-    ('-11.09', '21', False),
-    ('-11.09', '22', True),
-    ('-11.09', '23', True),
-    ('-133.98', '28', False)
-])
-def test_simple_rounding_half_up(mean, n, expected_consistency):
+            mean = test_data.iloc[row_index]['mean'].strip('"')
+            n = test_data.iloc[row_index]['n'].strip('"')
+            expected_consistency = test_data.iloc[row_index][rounding_type]
 
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_HALF_UP)
+            test_sets.append({"mean": mean,
+                              "n": n,
+                              "expected_consistency": expected_consistency,
+                              "rounding_type": rounding_type})
+
+    return test_sets
 
 
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', True),
-    ('11.09', '21', False),
-    ('11.09', '22', True),
-    ('133.98', '28', False),
-    ('-11.67', '3', True),
-    ('-11.66', '3', False),
-    ('-11.6667', '3', True),
-    ('-11.6666', '3', False),
-    ('-11.09', '21', False),
-    ('-11.09', '22', True),
-    ('-133.98', '28', False)
-])
-def test_simple_rounding_half_down(mean, n, expected_consistency):
-
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_HALF_DOWN)
-
-
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', True),
-    ('11.09', '21', False),
-    ('11.09', '22', False),
-    ('133.98', '28', False),
-    ('-11.67', '3', False),
-    ('-11.66', '3', True),
-    ('-11.6667', '3', False),
-    ('-11.6666', '3', True),
-    ('-11.09', '21', True),
-    ('-11.09', '22', True),
-    ('-133.98', '28', False)
-])
-def test_simple_rounding_ceiling(mean, n, expected_consistency):
-
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_CEILING)
-
-
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-   ('11.67', '3', False),
-   ('11.09', '21', True),
-   ('11.09', '22', True),
-    ('133.98', '28', False),
-    ('-11.67', '3', True),
-    ('-11.66', '3', False),
-    ('-11.6667', '3', True),
-    ('-11.6666', '3', False),
-    ('-11.09', '21', False),
-    ('-11.09', '22', False),
-    ('-133.98', '28', False),
-    ('6.10', '121', True)
-])
-def test_simple_rounding_floor(mean, n, expected_consistency):
-
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_FLOOR)
-
-
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', False),
-    ('11.09', '21', True),
-    ('11.09', '22', True),
-    ('133.98', '28', False),
-    ('-11.67', '3', False),
-    ('-11.66', '3', True),
-    ('-11.6667', '3', False),
-    ('-11.6666', '3', True),
-    ('-11.09', '21', True),
-    ('-11.10', '200', True),
-    ('-11.09', '111', True),
-    ('-133.98', '28', False),
-    ('6.05', '121', False),
-    ('6.10', '121', False)
-])
-def test_simple_rounding_05up(mean, n, expected_consistency):
-
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_05UP)
-
-
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', True),
-    ('11.09', '21', False),
-    ('11.09', '22', True),
-    ('133.98', '28', False),
-    ('-11.67', '3', True),
-    ('-11.66', '3', False),
-    ('-11.6667', '3', True),
-    ('-11.6666', '3', False),
-    ('-11.09', '21', False),
-    ('-11.10', '200', True),
-    ('-11.09', '111', True),
-    ('-133.98', '28', False),
-    ('6.05', '121', True),
-    ('6.10', '121', True),
-    ('3.5', '9', False),
-    ('1.600', '2000', True)
-])
-def test_simple_rounding_half_even(mean, n, expected_consistency):
-
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_HALF_EVEN)
-
-
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', True),
-    ('11.09', '21', False),
-    ('11.09', '22', False),
-    ('133.98', '28', False),
-    ('-11.67', '3', True),
-    ('-11.66', '3', False),
-    ('-11.6667', '3', True),
-    ('-11.6666', '3', False),
-    ('-11.09', '21', False),
-    ('-11.10', '200', True),
-    ('-11.09', '111', True),
-    ('-133.98', '28', False),
-    ('6.05', '121', True),
-    ('6.10', '121', True),
-    ('3.5', '9', True),
-    ('1.600', '2000', True)
-])
-def test_simple_rounding_up(mean, n, expected_consistency):
-
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_UP)
-
-
-@pytest.mark.parametrize("mean, n, expected_consistency", [
-    ('11.67', '3', False),
-    ('11.09', '21', True),
-    ('11.09', '22', True),
-    ('133.98', '28', False),
-    ('-11.67', '3', False),
-    ('-11.66', '3', True),
-    ('-11.6667', '3', False),
-    ('-11.6666', '3', True),
-    ('-11.09', '21', True),
-    ('-11.10', '200', True),
-    ('-11.09', '111', True),
-    ('-133.98', '28', False),
-    ('6.05', '121', True),
-    ('6.10', '121', True),
-    ('3.5', '9', True),
-    ('1.600', '2000', True)
-])
-def test_simple_rounding_down(mean, n, expected_consistency):
-
-    check_consistency(mean, n, expected_consistency, decimal.ROUND_DOWN)
+def test__csv(data_loader):
+    print(data_loader)
+    for data_set in data_loader:
+        print(data_set)
+        check_consistency(data_set["mean"],
+                          data_set["n"],
+                          data_set["expected_consistency"],
+                          getattr(decimal, data_set["rounding_type"]))
